fd147c450b0165e1945ba59ef33cea5eb8eca9de
irtaimal <ir.taimal10@uniandes.edu.co>
2018-04-18 20:42:45 -0500

    Initial commit

--- webContent/app/dashboard-route/dashboard-route.module.ts
+++ webContent/app/dashboard-route/dashboard-route.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DashboardRouteComponent} from './dashboard-route.component';
+import {CommonComponentsModule} from "../common-components/common-components.module";
 
 @NgModule({
     imports: [
-        CommonModule
+        CommonModule,
+        CommonComponentsModule
     ],
     exports: [
         DashboardRouteComponent
