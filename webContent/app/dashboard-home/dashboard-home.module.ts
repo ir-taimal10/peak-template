f0d502e46de4ce10e1b8fa8919a505d7ceab640f
irtaimal <ir.taimal10@uniandes.edu.co>
2018-04-18 23:40:02 -0500

    Initial commit

--- webContent/app/dashboard-home/dashboard-home.module.ts
+++ webContent/app/dashboard-home/dashboard-home.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DashboardHomeComponent} from './dashboard-home.component';
 import {CommonComponentsModule} from "../common-components/common-components.module";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
     imports: [
         CommonModule,
-        CommonComponentsModule
+        CommonComponentsModule,
+        RouterModule
     ],
     exports: [
         DashboardHomeComponent
